28e49915ec3ac88e3e50e52c63831b1a33745999
atsybulskiy <atsybulskiy.work@gmail.com>
2023-06-24 20:48:12 +0300

    Add new auth_api url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const API_URL = 'http://localhost:5001';
-export const API_AUTH_URL = 'http://localhost:5000/api';
+export const API_AUTH_URL = 'https://atsybulskiy.com/api';
+// export const API_AUTH_URL = 'https://node-server-i0x2.onrender.com/api';
+// export const API_AUTH_URL = 'http://localhost:5000/api';
 
 const $api = axios.create({
   baseURL: API_URL
